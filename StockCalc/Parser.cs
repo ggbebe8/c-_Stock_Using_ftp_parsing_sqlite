18f5c3080996db4257424e246c7dbc7e7d2c93f4
chops_pemtron <ggbebe7@gmail.com>
2021-01-08 16:23:11 +0900

    resolve proble that can not crowling

--- StockCalc/Parser.cs
+++ StockCalc/Parser.cs
@@ -222,6 +222,7 @@ namespace StockCalc
             try
             {
                 HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                request.UserAgent = @"Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1667.0 Safari/537.36";
                 HttpWebResponse response = (HttpWebResponse)request.GetResponse();
                 StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.Default);
                 String strHtml = reader.ReadToEnd();
